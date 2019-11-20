8e27aede81a926758308abab9aa8fd63db7a4e6c
McCullagh Lab <martin.mccullagh@colostate.edu>
2019-11-20 11:53:03 -0700

    upudated hf_routines for python 3

--- notes/hf_routines.py
+++ notes/hf_routines.py
@@ -183,7 +183,7 @@ def constructDensityMat(C):
     P = np.zeros((M,M),dtype=float)
     for i in range(M):
         for j in range(i,M):
-            for a in range(M/2):
+            for a in range(M//2):
                 P[i,j] += C[i,a]*C[j,a]
             P[i,j] *= 2.0
             P[j,i] = P[i,j]
