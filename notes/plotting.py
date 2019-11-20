914871f71ef62bab62b6cc718fd5867ed02223bd
McCullagh Lab <martin.mccullagh@colostate.edu>
2019-11-20 11:41:40 -0700

    added plotting

---
+++ notes/plotting.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+
+# define function for making figures
+def define_figure(xlabel="X",ylabel="Y"):
+    # setup plot parameters
+    fig = plt.figure(figsize=(10,8), dpi= 80, facecolor='w', edgecolor='k')
+    ax = plt.subplot(111)
+    ax.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
+    ax.set_xlabel(xlabel,size=20)
+    ax.set_ylabel(ylabel,size=20)
+    plt.tick_params(axis='both',labelsize=20)
+    return ax
